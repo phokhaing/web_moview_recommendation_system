2e8a4ee5df72d802c577657e4ad94ffbee2852ea
Pho Khaing <khaing.pho1991@gmail.com>
2022-10-27 17:25:52 +0700

    update admin list data

--- recommend/admin.py
+++ recommend/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
 from .models import Movie, Myrating, MyList
 
+
 # Register your models here.
-admin.site.register(Movie)
-admin.site.register(Myrating)
-admin.site.register(MyList)
+# admin.site.register(Movie)
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['title', 'genre', 'movie_logo']
+    list_display_links = ['title', 'genre']
+
+# admin.site.register(Myrating)
+@admin.register(Myrating)
+class MyratingAdmin(admin.ModelAdmin):
+    list_display = ['user', 'movie', 'rating']
+    list_display_links = ['user', 'movie']
+
+
+# admin.site.register(MyList)
+@admin.register(MyList)
+class MyListAdmin(admin.ModelAdmin):
+    list_display = ['user', 'movie', 'watch']
+    list_display_links = ['user', 'movie']
